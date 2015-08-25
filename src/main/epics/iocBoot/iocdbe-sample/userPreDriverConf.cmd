f8d7af1f97a606962b2218acaa1c180d2571ac0f
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2015-08-25 15:54:57 +0200

    First modifications during the test

--- src/main/epics/iocBoot/iocdbe-sample/userPreDriverConf.cmd
+++ src/main/epics/iocBoot/iocdbe-sample/userPreDriverConf.cmd
@@ -22,5 +22,5 @@
 #<recvTimeout>   : time to wait for input before disconnecting [ms]
 #<sendIntervall> : time to wait before sending new data to PLC [ms]
 
-#s7plcConfigure PLC:DBE,192.168.1.248,2000,172,20,1,1000,500
-s7plcConfigure PLC:DBE,127.0.0.1,2000,172,20,1,2000,500
+s7plcConfigure PLC:DBE,192.168.1.248,2000,172,20,1,1000,500
+#s7plcConfigure PLC:DBE,127.0.0.1,2000,172,20,1,2000,500
